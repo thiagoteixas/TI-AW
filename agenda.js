2473cafa558ab487ea741490a19de280b57a0f30
Thiago Teixeira Oliveira <thiagoteixas@gmail.com>
2022-11-13 22:44:07 -0300

    Inicializando o repositório

---
+++ agenda.js
@@ -0,0 +1,20 @@
+const elemento = {
+  tarefas: [
+    {
+      userId: 0,
+      id: 0,
+      nome: 'Aniversário',
+      info: 'Niver',
+      done: 0,
+    },
+    {
+      userId: 0,
+      id: 1,
+      nome: 'Reunião',
+      info: 'Reunião com a turma',
+      done: 0,
+    },
+  ],
+};
+
+export default elemento;
---
+++ mockData.js
@@ -0,0 +1,28 @@
+export const mockData = {
+  tarefas: [
+    {
+      id: 0,
+      nome: 'Aniversário',
+      info: 'Niver',
+      done: 0,
+    },
+    {
+      id: 1,
+      nome: 'Reunião',
+      info: 'Reunião com a turma',
+      done: 0,
+    },
+    {
+      id: 2,
+      nome: 'Festa de fim de ano',
+      info: 'Rua python 235',
+      done: 0,
+    },
+    {
+      id: 3,
+      nome: 'Entregar relatorio',
+      info: 'Até 23:20',
+      done: 1,
+    },
+  ],
+};
